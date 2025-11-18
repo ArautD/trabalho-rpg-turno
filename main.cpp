98a224126be6971d4323951994955c03cfed8565
ArautD <sergiosg6@yahoo.com>
2025-11-18 16:07:27 -0300

    feat: Adicao de guerreiro

--- main.cpp
+++ main.cpp
@@ -11,6 +11,12 @@ class Personagem{
         virtual ~Personagem() = default;
         virtual void atacar(Personagem& alvo) = 0;
         virtual void especial(Personagem& alvo) = 0;
+        virtual void defender() {
+        std::cout << nome << " está defendendo!\n";
+    }
+    virtual void fugir() {
+        std::cout << nome << " fugiu da batalha!\n";
+    }
 
         void receberDano(int dano){
             pontosVida -= dano;
@@ -20,18 +26,58 @@ class Personagem{
 
 class Guerreiro : public Personagem{
     public:
-        int coragem;
+        int coragem = 2;
+        bool covarde = false;
 
-        void atacar() override{
-            int dano = ataque - alvo.defesa;
-        } 
-        void especial() override {
+        void sttCovarde (){
+            std::cout << "O guerreiro se encontra covarde e perderá seu próximo turno\n";
         }
+
+        void atacar(Personagem& alvo) override{
+
+            if (covarde) {
+                sttCovarde();
+                covarde = false;
+                coragem = 2;
+                return;
+            }
+            int dano = forca - alvo.defesa;
+            if ( dano < 0) dano = 0;
+            alvo.receberDano(dano);
+
+            std:: cout << "o Ataque causou " << dano << " de dano\n";
+        } 
+
+        void especial(Personagem& alvo) override {
+
+            if (covarde){
+                sttCovarde();
+                return;
+            }
+            if (coragem > 0){
+                int dano = (forca * 2) - (alvo.defesa /2);
+                if (dano < 0) dano = 0;
+                alvo.receberDano(dano);
+                coragem --;
+
+                std::cout << nome << " usou ataque corajoso causando " << dano << " /Coragem Restante: " << coragem << "\n";
+
+                if (coragem == 0){
+                    covarde = true;
+                    sttCovarde();
+                }
+            } else {
+                std::cout << "Você não tem coragem sucificiente! \n";
+            }
+            
+        }
+
 };
 
 class Mago : public Personagem{
     public:
         int pontosMagia;
+    
 };
 
 class Arqueiro : public Personagem{
@@ -39,9 +85,64 @@ class Arqueiro : public Personagem{
         int destreza;
 };
 
+class Inimigo : public Personagem {
+public:
+    std::string tipo; // exemplo: "Orc", "Goblin", "Dragão"
 
+    Inimigo(std::string nome, int vida, int forca, int defesa, std::string tipo)
+        : tipo(tipo) {
+        this->nome = nome;
+        this->pontosVida = vida;
+        this->forca = forca;
+        this->defesa = defesa;
+    }
+
+    void atacar(Personagem& alvo) override {
+        int dano = forca - alvo.defesa;
+        if (dano < 0) dano = 0;
+        alvo.receberDano(dano);
+        std::cout << tipo << " " << nome << " atacou causando " << dano << " de dano!\n";
+    }
+
+    void especial(Personagem& alvo) override {
+        int dano = (forca * 2) - alvo.defesa;
+        if (dano < 0) dano = 0;
+        alvo.receberDano(dano);
+        std::cout << tipo << " " << nome << " usou ataque especial causando " << dano << " de dano!\n";
+    }
+};
+
+Inimigo criarOrc() {
+    return Inimigo("Gorg", 80, 15, 5, "Orc");
+}
+
+/*Guerreiro personagemEscolhido(std::string nome){
+            Guerreiro g;
+            g.nome = nome;
+            g.pontosVida = 20;
+            g.forca = 20;
+            g.defesa = 5;
+            g.coragem = 2;
+            g.covarde = false;
+            return g;
+        }*/
 
 int main(){
+
+    Guerreiro g;
+    g.nome = "Thor";
+    g.pontosVida = 100;
+    g.forca = 20;
+    g.defesa = 5;
+
+    Inimigo orc = criarOrc();
+    system("cls");
+    std::cout << " ===================================================\n ";
+    g.atacar(orc);
+    orc.atacar(g);
+    g.especial(orc);
+    orc.especial(g);
+    std::cout << " ===================================================\n ";
 
 
     return 0;
